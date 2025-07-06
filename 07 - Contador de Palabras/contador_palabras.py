33231d991bbd48ef14ce323ed8169c1327915b38
Jose Galvez <jlgalvez@gmail.com>
2025-07-06 18:10:35 +0200

    7-Contador de Palabras

--- 07 - Contador de Palabras/contador_palabras.py
+++ 07 - Contador de Palabras/contador_palabras.py
@@ -1,1 +1,15 @@
 # Contador de palabras: Pide al usuario que ingrese un texto y cuenta el número de palabras.
+'''
+    Titulo: Contador de palabras
+    Descripción: Pide al usuario que ingrese un texto y cuenta el número de palabras.
+    Programador: José Gálvez
+    Fecha: 06/07/2025
+'''
+def contar_palabras(texto):
+    palabras = texto.split()
+    return len(palabras)
+
+if __name__ == "__main__":
+    texto_usuario = input("Introduce un texto: ")
+    numero_palabras = contar_palabras(texto_usuario)
+    print(f"El número de palabras en el texto es: {numero_palabras}")
